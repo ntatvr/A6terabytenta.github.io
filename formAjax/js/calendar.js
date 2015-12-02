80c321af3b508cd3c22988014e7fcfb1b2e5ff8a
A6terabytenta <ntatvr@gmail.com>
2015-12-02 17:17:04 +0700

    Update Calendar and FormAjax
    show previous month and next month

--- formAjax/js/calendar.js
+++ formAjax/js/calendar.js
@@ -27,195 +27,226 @@ function init() {
 
 /*
 	create: button previous year, button next year, button previous month, button next month, select month and select year
-*/
-function drawRowAction() {
-	document.write("<TR ID='rowAction'>");
-	document.write("<TD CLASS='btn' onClick='btnPreviousYearClick();'><a TYPE='a' NAME='btnPreviousYear'>&#8647;</a></TD>");
-	document.write("<TD CLASS='btn' onClick='btnPreviousMonthClick();'><a TYPE='a' NAME='btnPreviousMonth'>&larr;</a></TD>");
-	document.write("<TD CLASS='btn' colspan='2'><SELECT ID='months' onChange='changeMonth();'>");
-	for(var i = 0; i < months.length; i++) {
-		document.write("<OPTION VALUE='" + i + "'>" + months[i] + "</OPTION>")
-	};
-	document.write("</SELECT></TD>")
-	document.write("<TD CLASS='btn' colspan='1'><SELECT ID='years' onChange='changeYear();'>");
-	for(var i = 1900; i <= 2025; i++) {
-		document.write("<OPTION VALUE='" + i + "'>" + i + "</OPTION>")
-	};
-	document.write("</SELECT></TD>")
-	document.write("<TD CLASS='btn' onClick='btnNextMonthClick();'><a TYPE='a' NAME='btnNextMonth'>&rarr;</a></TD>");
-	document.write("<TD CLASS='btn' onClick='btnNextYearClick();'><a TYPE='a' NAME='btnNextYear' >&#8649;</a></TD>");
-	document.write("</TR>");
-}
+	*/
+	function drawRowAction() {
+		document.write("<TR ID='rowAction'>");
+		document.write("<TD CLASS='btn' onClick='btnPreviousYearClick();'><a TYPE='a' NAME='btnPreviousYear'>&#8647;</a></TD>");
+		document.write("<TD CLASS='btn' onClick='btnPreviousMonthClick();'><a TYPE='a' NAME='btnPreviousMonth'>&larr;</a></TD>");
+		document.write("<TD CLASS='btn' colspan='2'><SELECT ID='months' onChange='changeMonth();'>");
+		for(var i = 0; i < months.length; i++) {
+			document.write("<OPTION VALUE='" + i + "'>" + months[i] + "</OPTION>")
+		};
+		document.write("</SELECT></TD>")
+		document.write("<TD CLASS='btn' colspan='1'><SELECT ID='years' onChange='changeYear();'>");
+		for(var i = 1900; i <= 2025; i++) {
+			document.write("<OPTION VALUE='" + i + "'>" + i + "</OPTION>")
+		};
+		document.write("</SELECT></TD>")
+		document.write("<TD CLASS='btn' onClick='btnNextMonthClick();'><a TYPE='a' NAME='btnNextMonth'>&rarr;</a></TD>");
+		document.write("<TD CLASS='btn' onClick='btnNextYearClick();'><a TYPE='a' NAME='btnNextYear' >&#8649;</a></TD>");
+		document.write("</TR>");
+	}
 
 /*
 	create calendar
-*/
-function drawCalendar(month, year) {
-	var count = 1;
-	for(var i = 0; i < 6; i++) {
-		document.write("<TR onclick='cancelCalendar();'>");
-		for(var j = 0; j < 7; j++) {
-			document.write("<TD ID='item" + count + "' CLASS='item' onClick='checkDay(" + count + ");'></TD>");		
-			count++;		
+	*/
+	function drawCalendar(month, year) {
+		var count = 1;
+		for(var i = 0; i < 6; i++) {
+			document.write("<TR onclick='cancelCalendar();'>");
+			for(var j = 0; j < 7; j++) {
+				document.write("<TD ID='item" + count + "' CLASS='item' onClick='checkDay(" + count + ");'></TD>");		
+				count++;		
+			};
+			document.write("</TR>");
 		};
-		document.write("</TR>");
-	};
-	setData();
-	setMonths();
-	setYears();
-}
+		setData();
+		setMonths();
+		setYears();
+	}
 
 /*
 	erase all value in calendar 
-*/
-function erase() {
-	for(var i = 1; i <= 42; i++) {
-		document.getElementById("item" + i).innerHTML = "";
-		document.getElementById("item" + i).style.background = "none";
+	*/
+	function erase() {
+		for(var i = 1; i <= 42; i++) {
+			document.getElementById("item" + i).innerHTML = "";
+			document.getElementById("item" + i).style.background = "none";
+		}
 	}
-}
 
 /*
 	set date for calendar
-*/
-function setData() {
-	var dateNow = new Date().getDate();
-	var month_now = new Date().getMonth();
-	var yearNow = new Date().getFullYear();
-	var day = new Date(yearCheck, monthCheck, 1).getDay();
+	*/
+	function setData() {
+		var dateNow = new Date().getDate();
+		var month_now = new Date().getMonth();
+		var yearNow = new Date().getFullYear();
+		var day = new Date(yearCheck, monthCheck, 1).getDay();
 
-	if(((yearCheck % 4 == 0) && (yearCheck % 100 != 0)) || (yearCheck % 400 == 0)) {
-		numberDayOfMonth[1] = 29; 
-	}else {
-		numberDayOfMonth[1] = 28;
-	}
+		var previous_month = monthCheck - 1;
+		var next_month = monthCheck + 1;
 
-	var numberDay = numberDayOfMonth[monthCheck];
-	var position = 1;
-	var count = 1;
-	var check = false;
-	for(var i = 0; i < 6; i++) {
-		for(var j = 0; j < 7; j++) {
-			if(count >= day) {
-				check = true;
-			}
-			if(check == true && position <= numberDay) {
-				document.getElementById("item" + (position + day)).innerHTML = position;
-				document.getElementById("item" + (position + day)).style.background = "#FBFCFC";
-				if(dateNow == position && month_now == monthCheck && yearNow == yearCheck) {
-					document.getElementById("item" + (position + day)).style.background = "#00ace6";
-					positionDateNow = "item" + (position + day);
+		if(((yearCheck % 4 == 0) && (yearCheck % 100 != 0)) || (yearCheck % 400 == 0)) {
+			numberDayOfMonth[1] = 29; 
+		}else {
+			numberDayOfMonth[1] = 28;
+		}
+
+		if(previous_month < 0){
+			previous_month = 11;
+		}
+
+		if(next_month == 12){
+			next_month = 0;
+		}
+		var numberDay = numberDayOfMonth[monthCheck];
+		var numberDayOfPreviousMonth = numberDayOfMonth[previous_month];
+		var numberDayOfNextMonth = numberDayOfMonth[next_month];
+
+		console.log(numberDayOfPreviousMonth);
+		console.log(numberDayOfNextMonth);
+
+		var position = 1;
+		var count = 1;
+		var check = false;
+		var positionStart = day;
+		var positionStop = (parseInt(numberDay) + parseInt(day));
+
+		for(var i = 0; i < 6; i++) {
+			for(var j = 0; j < 7; j++) {
+				if(count >= day) {
+					check = true;
 				}
-				position++;
+				if(check == true && position <= numberDay) {
+					console.log("date: "+ (position + day));
+					document.getElementById("item" + (position + day)).innerHTML = position;
+					document.getElementById("item" + (position + day)).style.background = "#FBFCFC";
+					if(dateNow == position && month_now == monthCheck && yearNow == yearCheck) {
+						document.getElementById("item" + (position + day)).style.background = "#00ace6";
+						positionDateNow = "item" + (position + day);
+					}
+					document.getElementById("item" + (position + day)).style.color = "#000";
+					position++;
+				}
+				count++;
 			}
-			count++;
+		}
+		for(var i = positionStart; i > 0; i--) {
+			console.log("i: "+i);
+			document.getElementById("item" + (i)).innerHTML = numberDayOfPreviousMonth--;
+			document.getElementById("item" + (i)).style.color = "#000";
+		}
+		for(var j = 1; j <= (42 - positionStop); j++) {
+			console.log("j: "+ (j + positionStop));
+			document.getElementById("item" + (j + positionStop)).innerHTML = j;
+			document.getElementById("item" + (j + positionStop)).style.color = "#000";
 		}
 	}
-}
 
-function checkDay(day) {
-	var day = document.getElementById("item" + day).innerHTML;
-	if(day != null && day != "") {
-		var results = day + "/" + (parseInt(document.getElementById("months").value) + 1) + "/" + yearCheck;
-		document.getElementById("output").value = results;
+	function checkDay(day) {
+		var day = document.getElementById("item" + day).innerHTML;
+		if(day != null && day != "") {
+			var results = day + "/" + (parseInt(document.getElementById("months").value) + 1) + "/" + yearCheck;
+			document.getElementById("output").value = results;
+		}
 	}
-}
 
-function changeMonth() {
-	erase();
-	monthCheck = document.getElementById("months").value;
-	console.log(monthCheck);
-	setData();
-}
+	function changeMonth() {
+		erase();
+		monthCheck = document.getElementById("months").value;
+		console.log(monthCheck);
+		setData();
+	}
 
-function setMonths() {
-	document.getElementById("months").value = monthCheck;
-}
+	function setMonths() {
+		document.getElementById("months").value = monthCheck;
+	}
 
-function setYears() {
-	document.getElementById("years").value = yearCheck;
-}
+	function setYears() {
+		document.getElementById("years").value = yearCheck;
+	}
 
-function changeYear() {
-	erase();
-	yearCheck = document.getElementById("years").value;
-	console.log(monthCheck);
-	setData();
-}
+	function changeYear() {
+		erase();
+		yearCheck = document.getElementById("years").value;
+		console.log(monthCheck);
+		setData();
+	}
 
 /*
 	create event for button previous month
-*/
-function btnPreviousMonthClick() {
-	erase();
-	monthCheck = monthCheck - 1;
-	if(monthCheck < 0) {
-		monthCheck = 11;
-		yearCheck = yearCheck - 1;
+	*/
+	function btnPreviousMonthClick() {
+		erase();
+		monthCheck = monthCheck - 1;
+		if(monthCheck < 0) {
+			monthCheck = 11;
+			yearCheck = yearCheck - 1;
+		}
+		console.log(monthCheck);
+		setData();
+		setMonths();
+		setYears();
 	}
-	console.log(monthCheck);
-	setData();
-	setMonths();
-	setYears();
-}
 
 /*
 	create event for button next month
-*/
-function btnNextMonthClick() {
-	erase();
-	monthCheck = monthCheck + 1;
-	if(monthCheck > 11) {
-		monthCheck = 0;
-		yearCheck = yearCheck + 1;
+	*/
+	function btnNextMonthClick() {
+		erase();
+		monthCheck = monthCheck + 1;
+		if(monthCheck > 11) {
+			monthCheck = 0;
+			yearCheck = yearCheck + 1;
+		}
+		console.log(monthCheck);
+		setData();
+		setMonths();
+		setYears();
 	}
-	console.log(monthCheck);
-	setData();
-	setMonths();
-	setYears();
-}
 
 /*
 	create event for button previous year
-*/
-function btnPreviousYearClick() {
-	erase();
-	yearCheck = yearCheck - 1;
-	console.log(monthCheck);
-	setData();
-	setMonths();
-	setYears();
-}
+	*/
+	function btnPreviousYearClick() {
+		erase();
+		yearCheck = yearCheck - 1;
+		console.log(monthCheck);
+		setData();
+		setMonths();
+		setYears();
+	}
 
 /*
 	create event for button next year
-*/
-function btnNextYearClick() {
-	erase();
-	yearCheck = yearCheck + 1;
-	console.log(monthCheck);
-	setData();
-	setMonths();
-	setYears();
-}
+	*/
+	function btnNextYearClick() {
+		erase();
+		yearCheck = yearCheck + 1;
+		console.log(monthCheck);
+		setData();
+		setMonths();
+		setYears();
+	}
 
-function drawDay() {
-	document.write("<TR ID='title'>");
-	for(var i = 0; i < day.length; i++) {
-		document.write("<TD>" + day[i] + "</TD>");
-	};
-	document.write("</TR>");
-}
+	function drawDay() {
+		document.write("<TR ID='title'>");
+		for(var i = 0; i < day.length; i++) {
+			document.write("<TD>" + day[i] + "</TD>");
+		};
+		document.write("</TR>");
+	}
 
-function setItem(item, value) {
-	var item = document.getElementById(item);
-	item.innerHTML = value;
-}
+	function setItem(item, value) {
+		var item = document.getElementById(item);
+		item.innerHTML = value;
+	}
 
-function cancelCalendar() {
-	document.getElementById("calendar").style.display = "none";
-}
+	function cancelCalendar() {
+		document.getElementById("calendar").style.display = "none";
+	}
 
-function openCalendar() {
-	document.getElementById("calendar").style.display = "block";
-}
+	function openCalendar() {
+		document.getElementById("calendar").style.display = "block";
+	}
